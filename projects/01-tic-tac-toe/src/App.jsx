f208dce9494c78cd802dfefc0cd17e42b6197153
Yilver7u7 <yilversgg@gmail.com>
2024-12-10 20:48:07 -0500

    Made the first visual part

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -1,10 +1,56 @@
+import { useState } from 'react'
 import './App.css'
 
+const TURNS = {
+  X: 'X',
+  O: 'O',
+}
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' :''}`
+  return (
+    <div className={className}>
+      {children}
+    </div>
+  )
+}
+
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
 
   return (
     <>
-    <h1>Tic tac toe</h1>
+    <main className='board'>
+      <h1>TIC TOC TOE</h1>
+      <section className='game'>
+        {
+          board.map((_, index) => {
+            return (
+              <div className='cell' key={index}>
+                <Square
+                key={index}
+                index={index}>
+                  {board[index]}
+                </Square>
+              </div>
+            )
+          })
+          }
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+
+
+    </main>
     </>
   )
 }
